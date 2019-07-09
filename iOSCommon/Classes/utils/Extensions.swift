3515b4b7aa954e793a7d8e6cf03304829d38d88a
Ricol Wang <ricol@opensimsim.com>
2019-07-09 16:03:09 +1000

    Compare dictionary and array.

--- iOSCommon/Classes/utils/Extensions.swift
+++ iOSCommon/Classes/utils/Extensions.swift
@@ -640,3 +640,64 @@ extension Bundle
         return (infoDictionary?["CFBundleVersion"] as? String) ?? ""
     }
 }
+
+func areEqual (_ left: Any, _ right: Any) -> Bool
+{
+    if  type(of: left) == type(of: right) &&
+        String(describing: left) == String(describing: right) { return true }
+    if let left = left as? [Any], let right = right as? [Any] { return left == right }
+    if let left = left as? [AnyHashable: Any], let right = right as? [AnyHashable: Any] { return left == right }
+    return false
+}
+
+extension Array where Element: Any
+{
+    static func == (left: [Element], right: [Element]) -> Bool
+    {
+        if left.count != right.count { return false }
+        for (index, leftValue) in left.enumerated()
+        {
+            guard areEqual(leftValue, right[index]) else { return false }
+        }
+        return true
+    }
+    
+    static func != (left: [Element], right: [Element]) -> Bool
+    {
+        return !(left == right)
+    }
+}
+extension Dictionary where Value: Any
+{
+    static func == (left: [Key : Value], right: [Key : Value]) -> Bool
+    {
+        if left.count != right.count { return false }
+        for element in left
+        {
+            guard   let rightValue = right[element.key],
+                areEqual(rightValue, element.value) else { return false }
+        }
+        return true
+    }
+    
+    static func != (left: [Key : Value], right: [Key : Value]) -> Bool
+    {
+        return !(left == right)
+    }
+}
+
+extension NSArray
+{
+    @objc func compare(array: NSArray) -> Bool
+    {
+        return (self as Array) == (array as Array)
+    }
+}
+
+extension NSDictionary
+{
+    @objc func compare(dictionary: NSDictionary) -> Bool
+    {
+        return (self as Dictionary) == (dictionary as Dictionary)
+    }
+}
