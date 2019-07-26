b74593ba297aa0fa3396e99adf09daf11f7abad4
Ricol Wang <ricol@opensimsim.com>
2019-07-26 12:42:16 +1000

    update.

--- iOSCommon/Classes/utils/Extensions.swift
+++ iOSCommon/Classes/utils/Extensions.swift
@@ -88,6 +88,45 @@ extension String
             return false
         }
     }
+    
+    public func trim() -> String
+    {
+        return self.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+    }
+    
+    public func expected_height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat
+    {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    public func expected_width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat
+    {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+}
+
+extension NSString
+{
+    @objc public func trim() -> NSString
+    {
+        return (self as String).trim() as NSString
+    }
+    
+    @objc public func expected_height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat
+    {
+        return (self as String).expected_height(withConstrainedWidth: width, font: font)
+    }
+    
+    @objc public func expected_width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat
+    {
+        return (self as String).expected_width(withConstrainedHeight: height, font: font)
+    }
 }
 
 extension UIView
